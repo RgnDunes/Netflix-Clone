696056830726fcd68a71493494d57c92815e2437
RgnDunes <singh.divyansh1802@gmail.com>
2021-04-27 05:47:31 +0530

    Added API call for TMDB

---
+++ src/Row.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      // console.table(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  // Whenever you use anything inside of a useEffect if there is any variable that is being pullid in from outside but it is used iside of the useEffect you have to put that variable in the condition part (second parameter in useEffect) in out case its fetchUrl.
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            className="row__poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Row;
