43320beeccfe85a225552e6f956c31769d0c7b09
RgnDunes <singh.divyansh1802@gmail.com>
2021-04-27 06:23:21 +0530

    Added Rows of images and thumbnails to frontend

---
+++ src/Banner.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  return <header></header>;
+}
+
+export default Banner;
--- src/Row.js
+++ src/Row.js
@@ -4,14 +4,14 @@ import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
       setMovies(request.data.results);
-      // console.table(request.data.results);
+      //   console.table(request.data.results);
       return request;
     }
     fetchData();
@@ -25,8 +25,11 @@ function Row({ title, fetchUrl }) {
       <div className="row__posters">
         {movies.map((movie) => (
           <img
-            className="row__poster"
-            src={`${base_url}${movie.poster_path}`}
+            key={movie.id}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
             alt={movie.name}
           />
         ))}
